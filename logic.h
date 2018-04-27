eaa4f54c7184e1c7a8da06174b1bcea4faa4a9c7
Webster Wing <webster.wing@gmail.com>
2018-04-27 14:41:16 -0500

    prototypes added to logic.h. Please call them from motiondetect_defs

--- logic.h
+++ logic.h
@@ -8,6 +8,14 @@ extern "C" {
 
     void bluetooth_react(char);
 
+    // each of these functions take a boolean value
+    // to determine if the sensor is too close.
+    // pass 0 if an obstacle is not detected and 1
+    // if one is visible
+    void left_tooClose(char);
+    void right_tooClose(char);
+    void rear_tooClose(char);
+
 #ifdef	__cplusplus
 }
 #endif
